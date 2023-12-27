3a0b6d34fa2f35ab9f73daa4e6755f5507921430
Anna-er <brungilda364@gmail.com>
2023-12-27 23:26:23 +0300

    feat: Add Yandex metric

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,28 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div dangerouslySetInnerHTML={{__html: 
+        `
+        <script type="text/javascript" >
+           (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+           m[i].l=1*new Date();
+           for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+           k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+           (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        
+           ym(96005530, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+           });
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/96005530" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        `
+        }}/>
+        {children}
+        </body>
     </html>
   )
 }
